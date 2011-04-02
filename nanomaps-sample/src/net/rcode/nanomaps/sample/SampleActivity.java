c4dc87ae5238245cbcf41a8aa884c1d71b20adaf
Stella Laurenzo <stella@laurenzo.org>
2011-04-02 06:58:30 -0700

    Initial commit

---
+++ nanomaps-sample/src/net/rcode/nanomaps/sample/SampleActivity.java
@@ -0,0 +1,13 @@
+package net.rcode.nanomaps.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
