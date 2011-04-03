71392861c029b54fb1f76b2e39bfb782de840d45
Stella Laurenzo <stella@laurenzo.org>
2011-04-03 15:40:57 -0700

    Mock tiles displaying properly

--- nanomaps-library/src/net/rcode/nanomaps/MapTileView.java
+++ nanomaps-library/src/net/rcode/nanomaps/MapTileView.java
@@ -3,7 +3,10 @@ package net.rcode.nanomaps;
 import net.rcode.nanomaps.MapTileSelector.TileKey;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.Log;
 import android.view.View;
 
@@ -15,6 +18,10 @@ import android.view.View;
  *
  */
 public class MapTileView extends View implements MapStateAware {
+	private static final int COLORS[] = new int[] {
+		Color.BLUE, Color.GREEN, Color.CYAN, Color.LTGRAY, Color.RED, Color.MAGENTA, Color.YELLOW
+	};
+	
 	private MapTileSelector selector;
 
 	public MapTileView(Context context, MapTileSelector selector) {
@@ -39,16 +46,46 @@ public class MapTileView extends View implements MapStateAware {
 		invalidate();
 	}
 	
+	/**
+	 * Given a MapState and TileKey, fill in a Rect with the pixel coordinates
+	 * of the tile for the current state.
+	 * This assumes rectangular display.  For rotation, this will all need to
+	 * be reworked.
+	 * @param mapState
+	 * @param tile
+	 */
+	static void mapTileToDisplay(MapState mapState, TileKey tile, Rect rect) {
+		double scaledSize=tile.size * tile.resolution / mapState.getResolution();
+		double left=mapState.projectedToDisplayX(tile.scaledX * tile.resolution) - mapState.getViewportOriginX();
+		double top=mapState.projectedToDisplayY(tile.scaledY * tile.resolution) - mapState.getViewportOriginY();
+		
+		rect.left=(int) Math.round(left);
+		rect.top=(int) Math.round(top);
+		rect.right=rect.left + (int) Math.ceil(scaledSize);
+		rect.bottom=rect.top + (int) Math.ceil(scaledSize);
+	}
+	
 	private class Blitter implements MapTileSelector.Handler {
+		private MapState mapState;
 		private Canvas canvas;
 		
-		public Blitter(Canvas canvas) {
+		public Blitter(MapState mapState, Canvas canvas) {
+			this.mapState=mapState;
 			this.canvas=canvas;
 		}
 
 		@Override
-		public void handleTile(TileKey key) {
-			Log.d(Constants.LOG_TAG, "Blitter tile: " + key);
+		public void handleTile(TileKey tile) {
+			Rect rect=new Rect();
+			mapTileToDisplay(mapState, tile, rect);
+			Log.d(Constants.LOG_TAG, "Blitter tile: " + tile + " -> " + rect);
+			
+			Paint paint=new Paint();
+			paint.setColor(COLORS[tile.hashCode() % COLORS.length]);
+			canvas.drawRoundRect(new RectF(rect), 10, 10, paint);
+			
+			paint.setColor(Color.BLACK);
+			canvas.drawText(tile.toString(), rect.left+5, rect.centerY(), paint);
 		}
 	}
 	
@@ -59,7 +96,7 @@ public class MapTileView extends View implements MapStateAware {
 		Rect bounds=canvas.getClipBounds();
 		Log.d(Constants.LOG_TAG, "MapTileView.onDraw(clip=" + canvas.getClipBounds() + ")");
 		
-		Blitter blitter=new Blitter(canvas);
+		Blitter blitter=new Blitter(mapState, canvas);
 		
 		selector.select(mapState.getProjection(),
 				mapState.getResolution(),
